595bdc4c0a5463188fe9289d64be8568880e89a5
nostradamus01 <stepanyan0299@gmail.com>
2023-05-03 22:31:40 +0400

    Good changes

--- src/components/tables/PlatformsTable.vue
+++ src/components/tables/PlatformsTable.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, computed } from 'vue'
-import { NDataTable, NButton, NSpin } from 'naive-ui'
+import { ref, computed, h } from 'vue'
+import { NDataTable, NButton, NSpin, NModal, NCard } from 'naive-ui'
 import { useDBStore } from '@/stores/dbStore'
 
 const columns = [{
@@ -19,7 +19,39 @@ const columns = [{
   title: "GPU",
   key: "gpu",
   resizable: true
+}, {
+  title: 'Action',
+  key: 'actions',
+  render(row) {
+    const buttons =  [
+      h(
+        NButton,
+        {
+          size: 'small',
+          onClick: () => EditRow(row)
+        },
+        { default: () => 'Edit' }
+      ),
+      h(
+        NButton,
+        {
+          size: 'small',
+          onClick: () => DeleteRow(row)
+        },
+        { default: () => 'Delete' }
+      )
+    ]
+    return h(
+      'div',
+      {
+        class: 'table-actions-conatiner'
+      },
+      buttons
+    )
+  }
 }]
+
+const showForm = ref(false);
 
 const isLoading = ref(false);
 
@@ -33,9 +65,38 @@ const data = computed(() => {
   return platforms;
 });
 
+const HandleAddClick = () => {
+  showForm.value = true;
+}
+
+const hideForm = () => {
+  showForm.value = false;
+}
+
+const submitForm = () => {
+  // ....
+  hideForm();
+}
+
 </script>
 
 <template>
+  <n-modal v-model:show="showForm" :mask-closable="false">
+    <n-card
+      style="width: 600px"
+      title="Add Platform"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      Content
+      <template #footer>
+        <n-button @click="hideForm" style="margin-right: 10px;">Close</n-button>
+        <n-button type="primary" @click="submitForm">Submit</n-button>
+      </template>
+    </n-card>
+  </n-modal>
   <n-button type="primary" @click="HandleAddClick" class="table-toolbar">
     Add Platform
   </n-button>
--- src/use/usePlatforms.js
+++ src/use/usePlatforms.js
@@ -3,6 +3,15 @@ import { useServer } from '@/use/useServer'
 export function usePlatforms() {
   const { req, dbStore } = useServer();
 
+  const getPlatforms = async () => {
+    const response = await req.get('Platforms/getAll');
+    if (response.status !== 200) {
+      alert("Something wrong!");
+      return null;
+    }
+    return await response.json();
+  }
+
   const addPlatform = async (data) => {
     const reqBody = {
       chipset: data.chipset,
@@ -18,17 +27,34 @@ export function usePlatforms() {
     return true;
   }
 
-  const getPlatforms = async () => {
-    const response = await req.get('Platforms/getAll');
+  const editPlatform = async (platformData) => {
+    const data = {
+      id: platformData.id,
+      chipset: platformData.chipset,
+      cpu: platformData.cpu,
+      gpu: platformData.gpu
+    }
+    const response = await putRequest('Platforms/edit', data);
     if (response.status !== 200) {
-      alert("Something wrong!");
       return null;
     }
-    return await response.json();
+    return true;
+  }
+
+  const deletePlatform = async (platformId) => {
+    const response = await deleteRequest('http://localhost:5169/api/Platforms/delete', platformId);
+    if (response.status !== 200) {
+      return null;
+    }
+    return true;
   }
 
   return {
     req,
-    dbStore
+    dbStore,
+    getPlatforms,
+    addPlatform,
+    editPlatform,
+    deletePlatform
   }
 }
