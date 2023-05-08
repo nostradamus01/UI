83fa15ae05e9f5edaab2be57b889171f3f3f4e45
nostradamus01 <stepanyan0299@gmail.com>
2023-05-08 23:16:54 +0400

    Areny hesa vrazi mej sa kavarti vortev inqy kara

--- src/components/tables/PhonesTable.vue
+++ src/components/tables/PhonesTable.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { ref, computed, reactive, h } from 'vue'
-import { NDataTable, NButton, NSpin } from 'naive-ui'
-import { useDBStore } from '@/stores/dbStore'
-import PhonesTableForm from '../forms/PhonesTableForm.vue';
+import { NForm, NSelect, NButton, NSpin, NDataTable } from 'naive-ui';
+import { ref, computed, h, reactive, onMounted, toRaw } from 'vue';
+import Modal from '@/components/Modal.vue';
+import { usePhoneDetails } from '@/use/usePhoneDetails';
 
 const columns = [{
   title: 'No',
   key: 'n',
   width: 60
 }, {
-  title: "Phone Detail Id",
+  title: "Phone Details",
   key: "phoneDetailId",
   resizable: true
 }, {
-  title: "Color Id",
+  title: "Color",
   key: "colorId",
   resizable: true
 }, {
-  title: "Storage Id",
+  title: "Storage",
   key: "storageId",
   resizable: true
 }, {
-  title: "RAM Id",
+  title: "RAM",
   key: "ramId",
   resizable: true
 }, {
@@ -33,7 +33,7 @@ const columns = [{
         NButton,
         {
           size: 'small',
-          onClick: () => EditRow(row)
+          onClick: () => editFn(row)
         },
         { default: () => 'Edit' }
       ),
@@ -41,7 +41,7 @@ const columns = [{
         NButton,
         {
           size: 'small',
-          onClick: () => DeleteRow(row)
+          onClick: () => deleteFn(row)
         },
         { default: () => 'Delete' }
       )
@@ -56,27 +56,118 @@ const columns = [{
   }
 }]
 
-const isLoading = ref(false);
+const { dbStore, getAll, add, edit, remove } = usePhoneDetails();
 
-const dbStore = useDBStore();
-
-const data = computed(() => {
-  const phones = dbStore.phones;
-  phones.forEach((phone, index) => {
-    phone.n = index + 1;
+const tableData = computed(() => {
+  const data = dbStore.phoneDetails;
+  data.forEach((element, index) => {
+    element.n = index + 1;
   });
-  return phones;
+  return data;
+});
+
+const isLoading = ref(false);
+const isFormLoading = ref(false);
+
+const form = reactive({
+  title: 'Add OS',
+  mode: 'add',
+  isVisible: false
+});
+
+const initialData = {
+  phoneDetails: null,
+  color: null,
+  storage: null,
+  ram: null
+}
+
+const data = reactive({ ...initialData });
+
+const oses = computed(() => {
+  const arr = []
+  dbStore.oses.forEach(os => {
+    arr.push({
+      label: os.name,
+      value: os.id
+    });
+  });
+  return arr;
+});
+
+const showForm = (row) => {
+  if (row) {
+    Object.assign(data, row);
+    form.mode = 'edit'
+    form.title = 'Edit Phone details'
+  } else {
+    Object.assign(data, initialData);
+    form.mode = 'add'
+    form.title = 'Add Phone details'
+  }
+  form.isVisible = true;
+}
+
+const hideForm = () => {
+  form.isVisible = false;
+}
+
+const getAllFn = async () => {
+  isLoading.value = true;
+  dbStore.phoneDetails = await getAll();
+  isLoading.value = false;
+}
+
+const editFn = (row) => {
+  showForm(row);
+}
+
+const removeFn = async (row) => {
+  isLoading.value = true;
+  await remove(row.id);
+  getAllFn()
+}
+
+const addFn = () => {
+  showForm();
+}
+
+const submit = async () => {
+  isFormLoading.value = true;
+  const obj = toRaw(data);
+  if (form.mode === 'edit') {
+    await edit(obj);
+  } else {
+    await add(obj);
+  }
+  close();
+  getAllFn();
+}
+
+const close = () => {
+  isFormLoading.value = false;
+  hideForm();
+}
+
+onMounted(async () => {
+  getAllFn();
 });
 
 </script>
 
 <template>
-  <PhonesTableForm />
+  <Modal :isVisible="form.isVisible" :title="form.title" @close="close" @submit="submit" :is-loading="isFormLoading">
+    <n-form ref="formRef" :model="data" class="my-form">
+      <n-select v-model:value="data.phoneDetails" :options="phoneDetails" placeholder="Brand" />
+      <n-select v-model:value="data.color" :options="colors" placeholder="Brand" />
+      <n-select v-model:value="data.storage" :options="storages" placeholder="Brand" />
+      <n-select v-model:value="data.ram" :options="rams" placeholder="Brand" />
+    </n-form>
+  </Modal>
+  <n-button type="primary" @click="addFn" class="table-toolbar">
+    Add Phone details
+  </n-button>
   <n-spin :show="isLoading">
-    <n-data-table :columns="columns" :data="data" />
+    <n-data-table :columns="columns" :data="tableData" />
   </n-spin>
 </template>
-
-<style>
-
-</style>
